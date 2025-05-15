fc4ec2e1fff974afec854626f0fde00b200b9eb6
Noah <nordqvist.gaming@gmail.com>
2025-05-15 21:28:35 +0200

    convert to godot 4.4, set up main scene and player movement

---
+++ lord-of-scraps/scenes/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+class_name Player
+
+@export var move_speed: float = 4.0
+
+
+func _ready() -> void:
+	print("Player ready")
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_up"):
+		position.y -= move_speed
+	if Input.is_action_pressed("move_down"):
+		position.y += move_speed
+	if Input.is_action_pressed("move_left"):
+		position.x -= move_speed
+	if Input.is_action_pressed("move_right"):
+		position.x += move_speed
