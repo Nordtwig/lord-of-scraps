6aa3a4b29c3eec9d2101281a0772e4144a30acc4
Noah <nordqvist.gaming@gmail.com>
2025-05-18 21:44:34 +0200

    refactor movement

--- lord-of-scraps/scenes/player.gd
+++ lord-of-scraps/scenes/player.gd
@@ -1,15 +1,14 @@
 extends CharacterBody2D
 class_name Player
 
-@export var move_speed: float = 4.0
+@export var move_speed: float = 100.0
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("move_up"):
-		position.y -= move_speed
-	if Input.is_action_pressed("move_down"):
-		position.y += move_speed
-	if Input.is_action_pressed("move_left"):
-		position.x -= move_speed
-	if Input.is_action_pressed("move_right"):
-		position.x += move_speed
+	_move()
+
+
+func _move() -> void:
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = move_vector * move_speed
+	move_and_slide()
